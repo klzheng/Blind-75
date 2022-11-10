5ed8271dd300cccdc62ddb0c59f91e982f1d3464
Kevin <klzheng@buffalo.edu>
2022-11-10 10:56:00 -0500

    347. Top K Frequent Elements - solved v2

--- topKFrequentElements-347.js
+++ topKFrequentElements-347.js
@@ -5,49 +5,57 @@
  */
 
 
-// fn -> takes in an array of nums and an integer k, return the k most frequent numbers in the nums arr
-// array, integer -> only numbers, array length >= 1, integer will always be in range btwn array length and 1
-// return array -> only numbers, at least 1 solution, length >= 1
+// fn -> array of numbers and an integer, k - return k numbers that are the most frequent
+// array, integer k -> numbers only, length >= 1, k > 0, array integers can be positive/negative
+// return integers -> array, length === k
+
+
+// [1,1,1,2,2,3], 2 -> [1,2]
+// [1], 1 -> [1]
 
 
 // initialize hashmap
-// initialize bucketSort
-// initialize result arr
-// loop through nums
-//      if nums[i] not in hashmap, create key-value pair
-//      if nums[i] in hashmap, increment value of key by 1
+// initialize bucket
+// initialize result
+// loop through nums arr
+//      make pairs in hashmap where the key == integer in nums, value == occurences in nums
+// loop through nums arr
+//      initialize empty subarrays in bucket
 // loop through hashmap
-//      match each hash's value to the bucket's index, and set the hash's key as the bucket's value
-// loop through the bucket in reverse order
-//      push each item in bucket to result arr
-//      break loop if result length === k 
+//      add each value to bucket where the map's keys === bucket's index
+// loop through bucket
+//      add to result array
+// slice result array (0,k)
 // return result
 
 
-var topKFrequent = function(nums, k) {
+var topKFrequent = function (nums, k) {
     const hash = new Map()
     const bucket = []
     const result = []
-    
+
     for (let i = 0; i < nums.length; i++) {
         hash.set(nums[i], (hash.get(nums[i]) || 0) + 1)
-    } 
-    
-    hash.forEach((value,key) => {
-        if (bucket[value]) {
-            bucket[value] = bucket[value].add(key)
-        } else {
-            bucket[value] = new Set().add(key)
-        }
-    })
-    
-    for (let i = bucket.length - 1; i >= 0; i--) {
-        if (bucket[i]) result.push(...bucket[i])
-        if (result.length === k) break
     }
-    
-    return result
-};
+
+    for (let i = 0; i < nums.length; i++) {
+        bucket.push([])
+    }
+
+
+    hash.forEach((value, key) => {
+        bucket[value - 1].push(key)
+    })
+
+
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (bucket[i].length !== 0) {
+            result.push(...bucket[i])
+        }
+    }
+
+    return result.slice(0, k)
+}
 
 // Time complexity: O(n)
 // Space complexity: O(n)
