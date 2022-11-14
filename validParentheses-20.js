d4b282577e9a727b6df548cc6441af272f8c656f
Kevin <klzheng@buffalo.edu>
2022-11-13 21:37:49 -0500

    #20 - Valid Parentheses - edited

--- validParentheses-20.js
+++ validParentheses-20.js
@@ -11,7 +11,7 @@
 
 // initialize stack
 // loop through the string
-//      if the string has a opening bracket, if it does push the corresponding closing bracket to the stack
+//      if the string has a opening bracket, push the corresponding closing bracket to the stack
 //      if string does not have opening bracket, check if string[i] === last item in stack, if true, remove last item in stack 
 //      else return false (b/c we know that the char is not an opening bracket, and the char does not === last item in stack)
 // return true if length of stack === 0, otherwise return false
