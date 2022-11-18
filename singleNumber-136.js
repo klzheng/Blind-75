652cf33f9433cbcdd97c4dc500a6204152c2ee74
Kevin <klzheng@buffalo.edu>
2022-11-18 10:01:06 -0500

    #136 - singleNumber - updated

--- singleNumber-136.js
+++ singleNumber-136.js
@@ -14,6 +14,7 @@
 // [1] -> 1
 
 
+// ~~~~~~ GOOD ~~~~~~~~
 // initialize hashmap
 // loop through nums arr
 //      add number and frequency as key-value pair to map
@@ -21,13 +22,13 @@
 //      return key that has value === 1
 
 
-var singleNumber = function (nums) {
+var singleNumber = function(nums) {
     let map = new Map()
-
+    
     for (let i = 0; i < nums.length; i++) {
         map.set(nums[i], (map.get(nums[i]) || 0) + 1)
     }
-
+    
     for (let [key, value] of map) {
         if (value === 1) return key
     }
@@ -35,3 +36,24 @@ var singleNumber = function (nums) {
 
 // Time complexity: O(N)
 // Space complexity: O(N)
+
+
+
+// ~~~~~ OPTIMAL ~~~~~~
+// initialize result var
+// loop through nums
+//      set result to the number XOR result, b/c if 2 numbers use the XOR operator, the result is 0, and if any number XOR 0, the result is the         number, so if every number appears twice except for 1 number, then all the duplicates will evaluate to 0, and 0 XOR unique number ==             unique number
+// return result
+
+
+var singleNumber = function(nums) {
+    let result
+    
+    for (let i = 0; i < nums.length; i++) {
+        result = nums[i] ^ result
+    }
+    return result
+}
+
+// Time complexity: O(N)
+// Space complexity: O(1)
